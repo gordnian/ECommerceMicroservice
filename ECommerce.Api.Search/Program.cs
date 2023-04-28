cb0182cbfdae34993cf217f4c28adcfda1dbc87d
= <=>
2023-04-28 17:30:56 -0500

    cleanup

--- ECommerce.Api.Search/Program.cs
+++ ECommerce.Api.Search/Program.cs
@@ -6,23 +6,28 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<ISearchService, SearchService>();
+
 builder.Services.AddHttpClient("OrdersService", conf =>
 {
     conf.BaseAddress = new Uri(builder.Configuration["Services:Orders"]!);
 });
+
 builder.Services.AddHttpClient("ProductsService", conf =>
 {
     conf.BaseAddress = new Uri(builder.Configuration["Services:Products"]!);
 })
     .AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
+
 builder.Services.AddHttpClient("CustomersService", conf =>
 {
     conf.BaseAddress = new Uri(builder.Configuration["Services:Customers"]!);  
 })
     .AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
+
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IProductsService, ProductsService>();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
